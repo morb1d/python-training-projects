60b76dae26ca0a1ecddc1991a119a5fe55cec632
Krzysztof Bury <krzysiek.bury@gmail.com>
2018-07-03 19:46:31 +0200

    Changes to port scanner.

--- PythonBootcampFinalProjects/port_scanner.py
+++ PythonBootcampFinalProjects/port_scanner.py
@@ -12,10 +12,10 @@ port_end = 65535
 net_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ip_address = input('Please, provide IP address - in format A.B.C.D: ')
-port_start = input('Please, provide starting port to scan: ')
-port_end = input('Please, provide ending port for scan: ')
+port_start = int(input('Please, provide starting port to scan: '))
+port_end   = int(input('Please, provide ending port for scan: '))
 
-for port in range(port_start, port_end):
+for port in range(port_start, port_end+1):
     scan_result = net_socket.connect_ex((ip_address, port))
 
     if (scan_result == 0):
