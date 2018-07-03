7b94762c86f34e2aa2f753efdc151e4a5c072b0b
Bury <212462648@logon.ds.ge.com>
2018-07-03 19:38:33 +0200

    adding new project files

---
+++ PythonBootcampFinalProjects/port_scanner.py
@@ -0,0 +1,24 @@
+## Port Scanner
+### Networking
+
+##Enter an IP address and a port range where the program will then attempt to find open ports on the given computer by connecting to each of them. On any successful connections ##mark the port as open.
+
+import socket
+
+ip_address = '127.0.0.1' # initialize as localhost
+port_start = 0
+port_end = 65535
+
+net_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_address = input('Please, provide IP address - in format A.B.C.D: ')
+port_start = input('Please, provide starting port to scan: ')
+port_end = input('Please, provide ending port for scan: ')
+
+for port in range(port_start, port_end):
+    scan_result = net_socket.connect_ex((ip_address, port))
+
+    if (scan_result == 0):
+        print('Port {} on machine {} is open.'.format(port, ip_address))
+    else:
+        print('Port {} on machine {} is closed.'.format(port, ip_address))
